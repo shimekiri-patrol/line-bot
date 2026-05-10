06f1f08db261ec47ce96aad2cec2b870285267f7
shimekiri-patrol <shiomisama@yahoo.co.jp>
2026-05-10 17:11:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(express.json());
 
 // LINE SDK 設定
 const config = {
-  channelAccessToken: process.env.MVKcpgZI5Uln/Yx7I6VJiDu9+3c/prYKkyd6dWMeW5TFH3PL3poxk9lm6o92a2XMAWq5KMmL66779IGisV2tU8dWTd83WR2gMvaZGJdHJsSAU5HCPjR3VtfqUfp+kLJzUl+XtrbQFzHWFOUu7LWOugdB04t89/1O/w1cDnyilFU=,
-  channelSecret: process.env.b9c8d83536f1b0f76fa19d6ebe7074b9
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
 };
 
 const client = new line.Client(config);
